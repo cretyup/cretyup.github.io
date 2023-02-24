937f54e6560a4a02a00ee8e6a0f670708ff0a3f5
cretyup <126222242+cretyup@users.noreply.github.com>
2023-02-23 22:59:04 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+  
+    var firebaseConfig = {
+    apiKey: "AIzaSyCKOJhjoO8NEQSWD1pNgVWrDLQ7A67y6TQ",
+    authDomain: "newbytecodew.firebaseapp.com",
+    databaseURL: "https://newbytecodew-default-rtdb.firebaseio.com/",
+    projectId: "newbytecodew",
+    storageBucket: "newbytecodew.appspot.com",
+    messagingSenderId: "30446772589",
+    appId: "1:30446772589:android:2c186aae32e5e071213a64"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
